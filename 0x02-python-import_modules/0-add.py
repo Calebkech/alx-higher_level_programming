86455c65af2dc8852067a84966145c996edd91d7
calebkech <calebkemboi61@gmail.com>
2023-11-30 18:43:20 +0300

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-a = 1
-b = 2
 # Import the add function from add_0.py
 from add_0 import add
+a = 1
+b = 2
 # Print the result using string formatting
 print("{} + {} = {}".format(a, b, add(a, b)))
