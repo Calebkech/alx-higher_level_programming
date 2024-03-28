6bb61df11c1bd5cba89ad201c948e32dab39af7d
caleb.kech <37027115+Calebkech@users.noreply.github.com>
2024-03-28 18:55:51 +0300

    final

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Get the byte size of the HTTP response header for a given URL.
-
-# Usage: ./script.sh <URL>
-
-# Use curl to fetch the URL and suppress output (-s).
-# Pipe the output to wc to count characters (-c).
 curl -s "$1" | wc -c
