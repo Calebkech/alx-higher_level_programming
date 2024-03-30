642fa9c3517d38560e6407576927400010ec2055
calebkech <calebkemboi61@gmail.com>
2024-03-30 05:31:55 -0700

    POST an email #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
+"""This script:
+- Accepts a URL as input.
+- Sends a request to the URL and retrieves the value
+- of the X-Request-Id variable from the response header.
 """
 import sys
 import urllib.request
