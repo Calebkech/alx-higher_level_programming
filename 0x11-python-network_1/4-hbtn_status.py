ee0364e4067bb599802c7c3c77c6046384810104
calebkech <calebkemboi61@gmail.com>
2024-03-30 05:47:35 -0700

    update fetch code

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""get https://intranet.hbtn.io/status."""
+"""A script that fetches https://alx-intranet.hbtn.io/status
+and displays the body of the response using the requests package.
+"""
+
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    response = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
+
